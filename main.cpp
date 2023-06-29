2fd93a2ece4ab85fb5e683fa6114b0482f130bb8
LitchiHotpot <948786580@qq.com>
2023-06-29 10:41:20 +0800

    Add method in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,27 @@
 #include "SudokuGenerator.h"
 using namespace std;
 
+//数独以vector<int>形式构建，如果需要生成多个数独，则用二维向量vector<vector<int>>
+
+//===================生成========================
+//在生成之前，需要构造生成器SudokuGenerator
+//对于generateSudoku方法，其输入参数为生成数独的个数，返回一个二维向量
+//对于generatePuzzles方法，其输入参数为(生成谜题个数,挖空数量),返回一个二维向量,其中挖空用0代替
+//挖空已经实现保证唯一解,如果挖空太多导致无法生成唯一解,会输出ERROR(这部分还要重写,目前思路是丢弃这一个终局)
+
+//===================解密========================
+//解密之前需要初始化解密器solver和DLX节点,代码如下
+//SudokuSolver solver;
+//DLXNode* listHead = new DLXNode();
+//之后可以调用solver的solveSudoku方法
+//输入参数DLXNode *listHead, vector<int>& sudoku, vector<int> &answer
+//第二个参数为求解的数独,第三个参数为生成的数独答案
+
+//TODO
+//文件读写
+//主函数命令行实现
+//测试(代码规范和覆盖率)
+//覆盖率可能很低,之后进行冗余代码删除
 
 int main() {
     SudokuSolver solver;
